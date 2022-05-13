3d90b17d707ca261933f966bbc4bdf2d4d079752
Sofia Fischer <sofia@philodev.com>
2022-05-13 10:54:53 +0200

    🧱 add deployment script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -6,4 +6,4 @@ echo $HETZNER_SSH_PASSWORD | sudo -kS rm -rf /var/www/html/philoblog/public/
 echo $HETZNER_SSH_PASSWORD | sudo -kS cp -r "$GITHUB_WORKSPACE/public" /var/www/html/philoblog/
 
 # fix permissions from sudo copy
-echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R www-data:www-data /var/www/html/philoblog/public
+echo $HETZNER_SSH_PASSWORD | sudo -kS chown -R www-data:www-data /var/www/html/philoblog/
